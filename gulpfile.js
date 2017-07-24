b15a48ac194e9764982418f0b576bb31f9776ef3
Samuel Kindler <sam@flamov.com>
2017-07-24 10:08:26 +0800

    Init commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp');
+var svgmin = require('gulp-svgmin');
+
+var source = './uncompressed/*.svg';
+var output = './compressed/';
+
+var defaultOptions = [
+	{ cleanupAttrs: true },
+	{ removeDoctype: true },
+	{ removeXMLProcInst: true },
+	{ removeComments: true },
+	{ removeMetadata: true },
+	{ removeTitle: true },
+	{ removeDesc: true },
+	{ removeUselessDefs: true },
+	{ removeEditorsNSData: true },
+	{ removeEmptyAttrs: true },
+	{ removeHiddenElems: true },
+	{ removeEmptyText: true },
+	{ removeEmptyContainers: true },
+	{ removeViewBox: true },
+	{ cleanupEnableBackground: true },
+	{ minifyStyles: true },
+	{ convertStyleToAttrs: true },
+	{ convertTransform: true },
+	{ removeUnknownsAndDefaults: true },
+	{ removeNonInheritableGroupAttrs: true },
+	{ removeUselessStrokeAndFill: true },
+	{ removeUnusedNS: true },
+	{ cleanupIDs: true },
+	{ collapseGroups: true },
+	{ removeRasterImages: true },
+	{ convertShapeToPath: true }
+];
+
+gulp.task('svg-normal', function() {
+
+	var optionsNormal = JSON.parse(JSON.stringify(defaultOptions));
+	optionsNormal.push({ mergePaths: false });
+
+	console.log(optionsNormal);
+
+	return gulp.src(source)
+		.pipe(svgmin({
+			plugins: optionsNormal
+		}))
+		.pipe(gulp.dest(output + '/normal'));
+
+});
+
+gulp.task('svg-merge', function() {
+
+	var optionsMerge = JSON.parse(JSON.stringify(defaultOptions));
+	optionsMerge.push({ mergePaths: true });
+
+	console.log(optionsMerge);
+
+	return gulp.src(source)
+		.pipe(svgmin({
+			plugins: optionsMerge
+		}))
+		.pipe(gulp.dest(output + '/merged'));
+
+});
+
+gulp.task('compress', [
+	'svg-normal',
+	'svg-merge',
+]);
